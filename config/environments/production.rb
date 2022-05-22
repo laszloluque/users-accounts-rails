3b096321945c7698f6c6921e961ea08bab8525da
Laszlo-VSC <laszloluque@gmail.com>
2022-05-22 22:59:04 +0200

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # config for devise
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://users-accounts-rails-2.herokuapp.com/', port: 80 }
 
 end
